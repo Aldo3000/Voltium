5e1dc7d91bcbf82c69d205012b91d50374e7e94c
Diana Cruz <dianacd1234567@gmail.com>
2026-08-17 21:26:53 -0600

    add tooltips

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const appendAlert = (message, type) => {
     const wrapper = document.createElement('div')
